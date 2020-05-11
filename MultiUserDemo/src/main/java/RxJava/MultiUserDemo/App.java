1863c00341b0a4f353025de7e8ee2ff599acff89
XYZ <xyz@gmail.com>
2020-05-10 18:57:39 -0700

    commiting from eclipse

--- MultiUserDemo/src/main/java/RxJava/MultiUserDemo/App.java
+++ MultiUserDemo/src/main/java/RxJava/MultiUserDemo/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "changes from eclipse" );
+    }
+    
+    public void localEclipse() {
+    	System.out.println("local eclipse");
     }
 }
