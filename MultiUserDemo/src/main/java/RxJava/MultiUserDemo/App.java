cccddf8c3efd91fe790165f9cdc6cd6a8d44179b
HDB <abc@gmail.com>
2020-05-10 19:02:49 -0700

    changes from sts

--- MultiUserDemo/src/main/java/RxJava/MultiUserDemo/App.java
+++ MultiUserDemo/src/main/java/RxJava/MultiUserDemo/App.java
@@ -8,6 +8,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello from sts" );
+    }
+    
+    public void localSts() {
+    	System.out.println("sts local");
+    }
+    
+    public void extraMethodFromSts() {
+    	System.out.println("extra method from sts");
     }
 }
